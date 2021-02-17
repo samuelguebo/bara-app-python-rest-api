49f9fcdfa5ca68ca66e5b58f5d2ea89faca1fb3a
samuelguebo <samuelguebo@gmail.com>
2021-02-17 00:30:51 +0000

    [TEST] Unit tests for AEJ Cron class

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -1,3 +1,4 @@
+from application.services.aej_cron import AEJCron
 from application.models.degree import Degree
 from application.services.atoo_cron import AtooCron
 import pytest
@@ -11,14 +12,7 @@ from application import db
 
 class TestCron:
 	"""Test for cron operations."""
-
-	def test_extract_degrees_edu(self):
-		url = 'https://emploi.educarriere.ci/offre-69336-office-manager.html'
-		content = Cron().extractContent(url, EducarriereCron.DETAILS_SELECTOR)
-		degrees = Cron().extractDegrees(content.upper())
-		print(degrees)
-		assert len(degrees) == 4
-
+	'''
 	def test_extract_type(self):
 		url = 'https://emploi.educarriere.ci/offre-69336-office-manager.html'
 		content = Cron().extractContent(url, EducarriereCron.DETAILS_SELECTOR)
@@ -32,10 +26,29 @@ class TestCron:
 		content = Cron().extractContent(url, selector)
 		assert len(content) > 100
 
+	def test_extract_degrees_edu(self):
+		url = 'https://emploi.educarriere.ci/offre-69336-office-manager.html'
+		content = Cron().extractContent(url, EducarriereCron.DETAILS_SELECTOR)
+		degrees = Cron().extractDegrees(content.upper())
+		print(degrees)
+		assert len(degrees) == 4
 	def test_extract_degrees_atoo(self):
 		url = 'http://www.atoo.ci/emploi/jobs/conseiller-technique-pme-h-f/'
 		content = Cron().extractContent(url, AtooCron.DETAILS_SELECTOR)
 		degrees = Cron().extractDegrees(content.upper())
 		print(degrees)
 		assert len(degrees) == 4
+	'''
+
+	
+	def test_extract_aej(self):
+		url = 'https://www.agenceemploijeunes.ci/site/offres-emplois/14316'
+		cron = AEJCron()
+		content = cron.extractContent(url, cron.DETAILS_SELECTOR)
+		degrees = cron.extractDegrees(content.upper())
+		type = cron.extractType(content.upper())
+		dates = cron.extract_dates(content)
+		print('{} {} {}'.format(degrees, type, dates))
+		assert len(degrees) == 1
 		
+	
